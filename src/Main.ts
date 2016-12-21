f0271652a73fe22b3e0b2b1e31435776149413ec
Andreas Engl <ae@cephea.de>
2016-12-21 13:58:19 +0100

    Restructured unit tests

---
+++ src/Main.ts
@@ -0,0 +1,15 @@
+import { BindWebSocket } from './BindWebSocket';
+import { CreateClassToComponent } from './CreateClassToComponent';
+import { RenameVariable } from './RenameVariable';
+import { AddVarToAllDeclarations } from './AddVarToAllDeclarations';
+import { PropTypeTemplate } from './PropTypeTemplate'
+import { Truthinator } from './Truthinator';
+
+export = [
+  BindWebSocket,
+  CreateClassToComponent,
+  RenameVariable,
+  AddVarToAllDeclarations,
+  PropTypeTemplate,
+  Truthinator
+];
