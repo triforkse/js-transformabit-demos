f79097c84e7bfb11c9104b5098b9ab795703467e
Andreas Engl <ae@cephea.de>
2017-01-05 17:48:47 +0100

    Improved repository setup

---
+++ .atomist/editors/Test.ts
@@ -0,0 +1,16 @@
+import { Project } from '@atomist/rug/model/Core';
+import { ProjectEditor } from '@atomist/rug/operations/ProjectEditor';
+import { Result, Status } from '@atomist/rug/operations/RugOperation';
+
+import { JsNode } from 'js-transformabit/dist/JsNode';
+
+let editor: ProjectEditor = {
+  tags: ['test'],
+  name: 'Test',
+  description: 'Test',
+  parameters: [],
+  edit(project: Project) {
+    project.println(JsNode.fromModuleCode('let foo = 42;').format());
+    return new Result(Status.Success);
+  }
+}
