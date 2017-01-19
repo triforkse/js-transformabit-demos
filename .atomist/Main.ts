d03d1b99d5d1357b87dd1144df908693e7ad1b25
Andreas Engl <ae@cephea.de>
2017-01-19 14:08:02 +0100

    Export all editors

---
+++ .atomist/Main.ts
@@ -0,0 +1,8 @@
+export * from './editors/AddPropTypes';
+export * from './editors/AddUnitTestForReactComponent';
+export * from './editors/AddWebSocket';
+export * from './editors/ConvertToReactClassComponent';
+export * from './editors/ConvertToReactComponent';
+export * from './editors/ExpressRest';
+// export * from './editors/Reformat';
+export * from './editors/Truthinator';
