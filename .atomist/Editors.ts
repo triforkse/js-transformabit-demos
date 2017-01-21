168eeaed6f0b6d16e61b36a9df8d57de6f67389d
Andreas Engl <ae@cephea.de>
2017-01-21 14:26:35 +0100

    Temporarily removed Reformat editor

--- .atomist/Editors.ts
+++ .atomist/Editors.ts
@@ -4,5 +4,5 @@ export * from './editors/AddUnitTestForReactComponent';
 export * from './editors/ConvertToReactClassComponent';
 export * from './editors/ConvertToReactComponent';
 // export * from './editors/ExpressRest';
-export * from './editors/Reformat';
+// export * from './editors/Reformat';
 export * from './editors/Truthinator';
